19da3f8a2ad024475b7d019d84a405ae989a0628
Azra Ali <azra_ali09@outlook.com>
2025-11-01 21:13:46 -0400

    created aboutme page (slowly learning about .jsx)

---
+++ react-app/src/aboutme.jsx
@@ -0,0 +1,11 @@
+
+function AboutMe() {
+    return (
+      <div className ="Homepage">
+        <h1>Hello! Welcome to my site!</h1>
+        <p>My name is Azra!</p>
+      </div>
+    )
+  }
+  
+  export default AboutMe
