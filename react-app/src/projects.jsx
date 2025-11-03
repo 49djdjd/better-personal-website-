643ffc6dc24bc8922a8c4a763bd7b25846ef7467
Azra Ali <azra_ali09@outlook.com>
2025-11-02 22:16:36 -0500

    fixed the spacing and working on adding the popup (changed the cursor too)

--- react-app/src/projects.jsx
+++ react-app/src/projects.jsx
@@ -1,24 +1,30 @@
 import './projects.css'
+import {useState} from 'react'
 function Projects() {
+    const [showPopup, setShowPopup] = useState(false);
+    function checkClick(){
+
+        setShowPopup(true);
+    }
     return (
     <div className ="projects">
         <h1>Hello! These are my past projects</h1>
         <ul className="project">
             <li className="p">
                 <p>Project 1</p>
-                <p>Personal Website</p>
+                <p className = "button" onClick={checkClick}>Personal Website</p>
             </li>
             <li className="p">
                 <p>Project 2</p>
-                <p>DBH Website</p>
+                <p className = "button" onClick={checkClick}>DBH Website</p>
             </li>
             <li className="p">
                 <p>Project 3</p>
-                <p>PAL Website</p>
+                <p className = "button" onClick={checkClick}>PAL Website</p>
             </li>
             <li className="p">
                 <p>Project 4</p>
-                <p>Virtual Pet</p>
+                <p className = "button" onClick={checkClick}>Virtual Pet</p>
             </li>
             
         </ul>
