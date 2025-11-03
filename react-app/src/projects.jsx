b07382b2fe74821795076fd3e1648bfadb971375
Azra Ali <azra_ali09@outlook.com>
2025-11-03 16:02:12 -0500

    created a popup for the personal website (need to make individual ones for each project)

--- react-app/src/projects.jsx
+++ react-app/src/projects.jsx
@@ -2,9 +2,10 @@ import './projects.css'
 import {useState} from 'react'
 function Projects() {
     const [showPopup, setShowPopup] = useState(false);
-    function checkClick(){
-
+    const [selectedProject, setSelectedProject] = useState('');
+    function checkClick(project){
         setShowPopup(true);
+        selectedProject(project);
     }
     return (
     <div className ="projects">
@@ -12,7 +13,8 @@ function Projects() {
         <ul className="project">
             <li className="p">
                 <p>Project 1</p>
-                <p className = "button" onClick={checkClick}>Personal Website</p>
+                <p className = "button" onClick={checkClick('Personal Website')}>Personal Website</p>
+                selectedProject = Personal Website
             </li>
             <li className="p">
                 <p>Project 2</p>
@@ -28,6 +30,13 @@ function Projects() {
             </li>
             
         </ul>
+            {showPopup && (
+            <div className = "popup">
+                <h1>Personal Website</h1>
+                <p>One of the first full fledge programs that I made using HTML, CSS, and JS. This program took me a while and while I am proud of it, I still see room for improvement!!</p>
+                <button>Close</button>
+            </div>
+        )}
     </div>
     )
   }
