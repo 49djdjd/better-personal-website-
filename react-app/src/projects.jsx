60c4d3f5a402d60eb0559cab08e2ecc2855ab544
Azra Ali <azra_ali09@outlook.com>
2025-11-08 21:24:22 -0500

    decided to just go with one big popup (rather than individual ones)

--- react-app/src/projects.jsx
+++ react-app/src/projects.jsx
@@ -13,20 +13,20 @@ function Projects() {
     return (
     <div className ="projects">
         <h1>Hello! These are my past projects</h1>
-        <ul className="project">
-            <li className="p" onClick={() => checkClick('Personal Website')} >
+        <ul className="project" onClick={() => setShowPopup(true)}>
+            <li className="p" >
                 <p>Project 1</p>
                 <p className = "button">Personal Website</p>
             </li>
-            <li className="p" onClick={() => checkClick('DBH Website')} >
+            <li className="p">
                 <p>Project 2</p>
                 <p className = "button">DBH Website</p>
             </li>
-            <li className="p" onClick={() => checkClick('PAL Website')}>
+            <li className="p">
                 <p>Project 3</p>
                 <p className = "button">PAL Website</p>
             </li>
-            <li className="p"onClick={() => checkClick('Virtual Pet')}>
+            <li className="p">
                 <p>Project 4</p>
                 <p className = "button">Virtual Pet</p>
             </li>
@@ -34,7 +34,7 @@ function Projects() {
         </ul>
             {showPopup && (
             <div className = "popup">
-                <h1>{selectedProject}</h1>
+                <h1>My Projects!</h1>
                 <p>One of the first full fledge programs that I made using HTML, CSS, and JS. This program took me a while and while I am proud of it, I still see room for improvement!!</p>
                 <button onClick={closePopup}>Close</button>
             </div>
